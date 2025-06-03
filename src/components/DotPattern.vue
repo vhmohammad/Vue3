363dff381235422856dd6fb8389307d39e6283e8
mohammad <mohammad.vh.pc@gmail.com>
2025-06-03 16:11:42 +0330

    feat ( DotPattern.vue ) creat page DotPattern

---
+++ src/components/DotPattern.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="pattern">
+    <div v-for="(line, index) in repeatedPattern" :key="index">
+      <pre>{{ line }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DotPattern",
+  data() {
+    return {
+      repeatCount: 5,
+    };
+  },
+  computed: {
+    repeatedPattern() {
+      const basePattern = [
+        ".",
+        "..",
+        "...",
+        "....",
+        "    .",
+        "   ..",
+        "  ...",
+        " ...."
+      ];
+      const result = [];
+      for (let i = 0; i < this.repeatCount; i++) {
+        result.push(...basePattern);
+      }
+      return result;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pattern {
+  font-family: monospace;
+  white-space: pre;
+}
+</style>
