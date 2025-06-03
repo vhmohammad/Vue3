0fbe0dc7a502a7c2781048bf04b2f5dc02cfc561
mohammad <mohammad.vh.pc@gmail.com>
2025-06-03 16:10:28 +0330

    feat ( App.vue ) Button DotPattern

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
 
-  <h1> Poroject Web </h1>
-
+  <h1 style="text-align: center; color: brown "> Poroject Web </h1>
+<div class="col-12">
   <router-link to="/TodoList">
-    <button type="button" class="btn btn-primary">
+    <button type="button" class="btn btn-outline-primary" style="padding: 5px; margin-right: 10px ">
       TodoList
     </button>
   </router-link>
-  <router-view></router-view>
 
+  <router-link to="/DotPattern">
+    <button type="button" class="btn btn-outline-danger" style="padding: 5px">
+      DotPattern
+    </button>
+  </router-link>
+
+  <router-view>
+
+  </router-view>
+</div>
 </template>
 
 <script setup>
