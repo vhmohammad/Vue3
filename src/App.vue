a1792d85be686fff26247391a1da298c812aa2bf
mohammad <mohammad.vh.pc@gmail.com>
2025-06-07 13:30:47 +0330

    feat ( App.vue ) Sort the code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
 
   <h1 style="text-align: center; color: brown "> Poroject Web </h1>
-<div class="col-12">
-  <router-link to="/TodoList">
-    <button type="button" class="btn btn-outline-primary" style="padding: 5px; margin-right: 10px ">
-      TodoList
-    </button>
-  </router-link>
+  <div class="col-12">
+    <router-link to="/TodoList">
+      <button type="button" class="btn btn-outline-primary" style="padding: 5px; margin-right: 10px ">
+        TodoList
+      </button>
+    </router-link>
 
-  <router-link to="/DotPattern">
-    <button type="button" class="btn btn-outline-danger" style="padding: 5px; margin-right: 10px ">
-      DotPattern
-    </button>
-  </router-link>
+    <router-link to="/DotPattern">
+      <button type="button" class="btn btn-outline-danger" style="padding: 5px; margin-right: 10px ">
+        DotPattern
+      </button>
+    </router-link>
 
-  <router-link to="/Fibonacci">
-    <button type="button" class="btn btn-outline-warning" style="padding: 5px">
-      FibonacciPattern
-    </button>
-  </router-link>
+    <router-link to="/Fibonacci">
+      <button type="button" class="btn btn-outline-warning" style="padding: 5px">
+        FibonacciPattern
+      </button>
+    </router-link>
 
-  <router-view>
+    <router-view>
 
-  </router-view>
-</div>
+    </router-view>
+  </div>
 </template>
 
 <script setup>
